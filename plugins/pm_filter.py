33faad5c58ff74e479ba5f2776d0cee20c9aa08f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 01:02:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1311,7 +1311,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         search = query.message.text 
         settings = await get_settings(query.message.chat.id)          
         imdb = await get_poster(search) if IMDB else None
-        await query.answer(imdb['title'], show_alert=True)
+        await query.answer(f"{imdb['title']}", show_alert=True)
 
 
 
