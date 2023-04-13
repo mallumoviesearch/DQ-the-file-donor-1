d68b2b2e85e07f4922a1b3114feb549341fa8caa
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 23:15:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-BOT
 fi
-cd /DQ-The-File-Donor
+cd /DQ-BOT
 pip3 install -U -r requirements.txt
 echo "Starting DQ-BOT...."
 python3 bot.py
